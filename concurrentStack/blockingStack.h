971530497989394dce270b0d2484e997392fa37e
Cubicuz <contactme@through.github>
2021-12-03 23:58:22 +0100

    add sanitizer, fix memleak

--- concurrentStack/blockingStack.h
+++ concurrentStack/blockingStack.h
@@ -18,6 +18,14 @@ public:
     root->next = nullptr;
     last = root;
   }
+  ~BlockingStack() {
+    while (!empty())
+    {
+      pop();
+    }
+    delete root;
+    root = nullptr;
+  }
   bool empty() const { return root->next == nullptr; }
   size_t size() const {
     std::lock_guard<std::mutex> lockguard(mutex);
@@ -36,6 +44,7 @@ public:
   }
   void push(const T &t) {
     auto ref = new stackframe;
+    ref->next = nullptr;
     ref->obj = t;
     std::lock_guard<std::mutex> lockguard(mutex);
     last->next = ref;
@@ -44,6 +53,7 @@ public:
   }
   void push(T &&t) {
     auto ref = new stackframe;
+    ref->next = nullptr;
     ref->obj = std::move(t);
     std::lock_guard<std::mutex> lockguard(mutex);
     last->next = ref;
@@ -57,10 +67,13 @@ public:
     if (root == last) {
       return {};
     }
-    std::lock_guard<std::mutex> lockguard(mutex);
-    auto ref = last;
-    ref->prev->next = nullptr;
-    last = ref->prev;
+    stackframe* ref;
+    {
+      std::lock_guard<std::mutex> lockguard(mutex);
+      ref = last;
+      ref->prev->next = nullptr;
+      last = ref->prev;
+    }
     auto obj = ref->obj;
     delete ref;
     return obj;
--- concurrentStack/test/test.cpp
+++ concurrentStack/test/test.cpp
@@ -5,28 +5,56 @@
 
 TEST_CASE("basic compile test") { REQUIRE(1 == 1); }
 
-TEST_CASE("blockingStack") {
+TEST_CASE("memory correctness")
+{
+  BlockingStack<int> bs;
+
+  const size_t entries = 10000;
+
+  bs.pop();
+  bs.pop();
+  volatile auto a = bs.size();
+  for (size_t i = 0; i < entries; i++)
+  {
+    bs.push(i);
+  }
+  a = bs.size();
+  for (int i = entries - 1; i >= 0; i--)
+  {
+    REQUIRE(bs.pop() == i);
+  }
+  a = bs.size();
+  bs.pop();
+  bs.pop();
+}
+
+TEST_CASE("blocking performance")
+{
 
   BlockingStack<int> bs;
 
   const size_t entries = 10000;
 
-  BENCHMARK_ADVANCED("blocking stack")(Catch::Benchmark::Chronometer meter) {
+  BENCHMARK_ADVANCED("blocking stack")
+  (Catch::Benchmark::Chronometer meter)
+  {
 
-    while (!bs.empty()) {
+    while (!bs.empty())
+    {
       bs.pop();
     }
 
-    meter.measure([&bs] {
+    meter.measure([&bs]
+                  {
       for (size_t i = 0; i < entries; i++) {
         bs.push(i);
-      }
-    });
+      } });
   };
 
   std::cout << "size " << bs.size();
 
-  for (int i = entries-1; i >= 0; i--) {
+  for (int i = entries - 1; i >= 0; i--)
+  {
     REQUIRE(bs.pop() == i);
   }
 }
