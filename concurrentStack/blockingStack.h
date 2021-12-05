a0d5d83402220b3e08ed757fd05fd21cb5d4f4f8
Cubicuz <contactme@through.github>
2021-12-05 10:38:43 +0100

    fix stack logic

--- concurrentStack/blockingStack.h
+++ concurrentStack/blockingStack.h
@@ -1,78 +1,79 @@
 #include <mutex>
 
-template <class T> class BlockingStack {
+template <class T>
+class BlockingStack
+{
 private:
-  struct stackframe {
-    stackframe *prev;
+  struct stackframe
+  {
     T obj;
     stackframe *next;
   };
-  stackframe *root;
-  stackframe *last;
+  stackframe *root{nullptr};
   mutable std::mutex mutex;
 
 public:
-  BlockingStack() {
-    root = new stackframe;
-    root->prev = nullptr;
-    root->next = nullptr;
-    last = root;
-  }
-  ~BlockingStack() {
+  ~BlockingStack()
+  {
     while (!empty())
     {
       pop();
     }
-    delete root;
-    root = nullptr;
   }
-  bool empty() const { return root->next == nullptr; }
-  size_t size() const {
+  bool empty() const { return root == nullptr; }
+  size_t size() const
+  {
     std::lock_guard<std::mutex> lockguard(mutex);
     stackframe *frame{root};
     size_t cnt{0};
-    while (frame->next != nullptr)
+    while (frame != nullptr)
     {
-        cnt++;
-        frame = frame->next;
+      cnt++;
+      frame = frame->next;
     }
     return cnt;
   }
-  T &top() const {
+  T &top() const
+  {
     std::lock_guard<std::mutex> lockguard(mutex);
-    return last->obj;
+    return root->obj;
   }
-  void push(const T &t) {
+  void push(const T &t)
+  {
     auto ref = new stackframe;
-    ref->next = nullptr;
     ref->obj = t;
     std::lock_guard<std::mutex> lockguard(mutex);
-    last->next = ref;
-    ref->prev = last;
-    last = ref;
+    ref->next = root;
+    root = ref;
   }
-  void push(T &&t) {
+  void push(T &&t)
+  {
     auto ref = new stackframe;
-    ref->next = nullptr;
     ref->obj = std::move(t);
     std::lock_guard<std::mutex> lockguard(mutex);
-    last->next = ref;
-    ref->prev = last;
-    last = ref;
+    ref->next = root;
+    root = ref;
   };
 
-  template <class... Args> decltype(auto) emplace(Args &&...args){};
+  template <class... Args>
+  decltype(auto) emplace(Args &&...args){};
 
-  T pop() {
-    if (root == last) {
+  T pop()
+  {
+    if (root == nullptr)
+    {
+      // or throw or so
       return {};
     }
-    stackframe* ref;
+    stackframe *ref;
     {
       std::lock_guard<std::mutex> lockguard(mutex);
-      ref = last;
-      ref->prev->next = nullptr;
-      last = ref->prev;
+      ref = root;
+      if (ref == nullptr){
+        // or throw or so
+        return {};
+      }
+      root = ref->next;
     }
     auto obj = ref->obj;
     delete ref;
